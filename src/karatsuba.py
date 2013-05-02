6652d41adfe2c0730333ed78aff9ac547062832a
David Bautista <davidandres@outlook.com>
2013-05-01 20:39:13 -0500

    Initial release

---
+++ src/karatsuba.py
@@ -0,0 +1,60 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        Karatsuba Algorithm Implementation
+#
+# Author:      David Bautista
+#
+# Created:     01/05/2013
+#-------------------------------------------------------------------------------
+
+import math
+
+#Returns the first m digits of a number
+def lhalf(c,m):
+    return int(str(c)[:m])
+
+#Returns the last m digits of a number    
+def rhalf(c,m):
+    return int(str(c)[-m:])
+
+def multp(a,b):
+    #If the numbers are too small, makes the multiplication directly
+    if (0 <= a < 10) or (0 <= b < 10):
+        return a*b
+        
+    elif (type(a) == int) and (type(b) == int) or (type(a) == long) and (type(b) == long):
+        #Computing the sign of the result
+        zs = int(math.copysign(1,a)*math.copysign(1,b))
+        
+        #Working with non-signed numbers
+        x = str(abs(a))
+        y = str(abs(b))
+        xl = len(x)
+        yl = len(y)
+        
+        #Making 'm' even
+        if max(xl,yl) & 1:
+            m = max(xl,yl)+1
+        else:
+            m = max(xl,yl)
+        
+        mh = m/2
+        
+        #Zero-filling strings to fit m-size
+        x = x.rjust(m,'0')
+        y = y.rjust(m,'0')
+            
+        x0 = rhalf(x,mh)
+        x1 = lhalf(x,mh)
+
+        y0 = rhalf(y,mh)
+        y1 = lhalf(y,mh)        
+
+        z0 = multp(x0,y0)
+        z1 = multp(x1+x0,y1+y0)
+        z2 = multp(x1,y1)
+        
+        return zs*((z2*(10**m))+((z1-z2-z0)*(10**(mh)))+z0)
+        
+    #Returns false if it's used a parameter type other than int or long
+    else:
+        return False
---
+++ src/testKaratsuba.py
@@ -0,0 +1,50 @@
+﻿import unittest
+import karatsuba
+import random
+
+#Invalid characters test
+class invalidtest(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(karatsuba.multp('a','b'),False)
+
+#Negative integers tests
+class negativetest(unittest.TestCase):
+    def test6(self):
+        a,b=random.randint(-99,-10),random.randint(10,99)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+    def test7(self):
+        a,b=random.randint(10,99),random.randint(-99,-10)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+    def test8(self):
+        a,b=random.randint(-99,-10),random.randint(-99,-10)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+        
+#Big integer test
+class bigtest(unittest.TestCase):
+    def test11(self):
+        a,b=random.randint(10000000000000000000000000000,1000000000000000000000000000000),random.randint(10000000000000000000000000000,1000000000000000000000000000000)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+
+#Different size numbers tests
+class diffsiztest(unittest.TestCase):
+    def test16(self):
+        a,b=random.randint(100000,999999),random.randint(1000,9999)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+    def test17(self):
+        a,b=random.randint(1000,9999),random.randint(100000,999999)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+    def test18(self):
+        a,b=random.randint(-99,-10),random.randint(-99,-10)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+
+#Odd sized numbers tests
+class oddsiztest(unittest.TestCase):
+    def test21(self):
+        a,b=random.randint(100,999),random.randint(100,999)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+    def test22(self):
+        a,b=random.randint(1000000,9999999),random.randint(1000000,9999999)
+        self.assertEqual(karatsuba.multp(a,b),a*b)
+
+if __name__ == '__main__':
+    unittest.main()
